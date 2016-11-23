226ca458a542d368cc6fd0b071925f4dbec3d487
Anton <pronovise@gmail.com>
2016-11-24 00:37:53 +0200

    Update ft_strequ_main.c

--- test/ft_strequ_main.c
+++ test/ft_strequ_main.c
@@ -28,21 +28,21 @@ int	main(void)
 	char	test4[100];
 
 	if (ft_strequ(test, test2))
-		printf ("F*ck you\n");
+		printf ("BAD\n");
 	if (ft_strequ(test, test3))
-		printf ("F*ck you\n");
+		printf ("BAD\n");
 	if (ft_strequ(test, test4))
-		printf ("F*ck you\n");
+		printf ("BAD\n");
 	if (ft_strequ(test2, test3))
-		printf ("F*ck you\n");
+		printf ("BAD\n");
 	if (ft_strequ(test2, test4))
-		printf ("F*ck you\n");
+		printf ("BAD\n");
 	if (ft_strequ(test3, test4))
-		printf ("F*ck me\n");
+		printf ("Good\n");
 	if (ft_strequ(test3, test3))
-		printf ("F*ck me\n");
+		printf ("Good\n");
 	if (ft_strequ(test4, test4))
-		printf ("F*ck me\n");
+		printf ("Good\n");
 	if (ft_strequ(test2, test2))
-		printf ("F*ck me\n");
+		printf ("Good\n");
 }
